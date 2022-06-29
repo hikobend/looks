fe412f4b8685e4a796a5aaa247fca88bfea47d1a
hikobend <hiko.bkend@gmail.com>
2022-06-29 22:38:38 +0900

    質問回答ページ遷移

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'questions/index'
   root to: 'top#index'
-  resources :ques1, only: [:index]
+  resources :questions, only: [:index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
