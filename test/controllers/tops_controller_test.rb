44d7925630d32a44194b215fb7c5cfec82e8ebd3
hikobend <hiko.bkend@gmail.com>
2022-07-01 21:30:50 +0900

    新しいトップページ作成

--- test/controllers/tops_controller_test.rb
+++ test/controllers/tops_controller_test.rb
@@ -1,8 +1,8 @@
 require "test_helper"
 
-class TopControllerTest < ActionDispatch::IntegrationTest
+class TopsControllerTest < ActionDispatch::IntegrationTest
   test "should get show" do
-    get top_show_url
+    get tops_show_url
     assert_response :success
   end
 end
