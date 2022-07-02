895a924cafeaef1c0f7d540b572144e1259c6160
hikobend <hiko.bkend@gmail.com>
2022-07-02 14:31:29 +0900

    名前作成後、画面遷移ページ作成

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -2,6 +2,10 @@ class TopsController < ApplicationController
   def top 
   end
   
+  def index
+    @tops = Top.all
+  end 
+
   def new
     @top = Top.new
   end
@@ -9,14 +13,14 @@ class TopsController < ApplicationController
   def create
     @top = Top.new(top_params)
     if @top.save
-      redirect_to questions_path, notice: '名前を追加しました'
+      redirect_to tops_path
     else
-      flash.now[:notice] = '追加に失敗しました'
       render :new
     end
   end
 
   private
+
   def top_params
     params.permit(:name)
   end
