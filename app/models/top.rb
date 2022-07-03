4e089a8f104ec5d19fe49f488d607fc9a0a710f8
hikobend <hiko.bkend@gmail.com>
2022-07-03 11:41:42 +0900

    model更新

--- app/models/top.rb
+++ app/models/top.rb
@@ -2,9 +2,9 @@
 
 class Top < ApplicationRecord
   validates :name, presence: true, length: { in: 1..60, allow_blank: true }
-  validates :ques_one, presence: true
-  validates :ques_two, presence: true
-  validates :ques_three, presence: true
-  validates :ques_four, presence: true
-  validates :ques_five, presence: true
+  validates :ques_one, inclusion: { in: [true, false] }
+  validates :ques_two, inclusion: { in: [true, false] }
+  validates :ques_three, inclusion: { in: [true, false] }
+  validates :ques_four, inclusion: { in: [true, false] }
+  validates :ques_five, inclusion: { in: [true, false] }
 end
