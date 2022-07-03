7c5f8f969528f9762b9db8872ab1e77c6196f96e
こうへい <92671446+hikobend@users.noreply.github.com>
2022-07-03 22:22:54 +0900

    Revert "Add/question/content"

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,65 +1,32 @@
-# 夜型 消極的 専門的 衝動的 真面目
-Answer.create!(id: 1, answer: 'オーロラ')
-# 夜型 消極的 専門的 衝動的 臨機応変
-Answer.create!(id: 2, answer: '流れ星')
-# 夜型 消極的 専門的 計画的 真面目
-Answer.create!(id: 3, answer: '街灯')
-# 夜型 消極的 専門的 計画的 臨機応変
-Answer.create!(id: 4, answer: '月')
-# 夜型 消極的 器用的 衝動的 真面目
-Answer.create!(id: 5, answer: 'コウモリ')
-# 夜型 消極的 器用的 衝動的 臨機応変
-Answer.create!(id: 6, answer: '夜光貝')
-# 夜型 消極的 器用的 計画的 真面目
-Answer.create!(id: 7, answer: 'フクロウ')
-# 夜型 消極的 器用的 計画的 臨機応変
-Answer.create!(id: 8, answer: '月')
-# 夜型 積極的 専門的 衝動的 真面目
-Answer.create!(id: 9, answer: 'アライグマ')
-# 夜型 積極的 専門的 衝動的 臨機応変
-Answer.create!(id: 10, answer: '反射板')
-# 夜型 積極的 専門的 計画的 真面目
-Answer.create!(id: 11, answer: '星')
-# 夜型 積極的 専門的 計画的 臨機応変
-Answer.create!(id: 12, answer: 'シーラカンス')
-# 夜型 積極的 器用的 衝動的 真面目
-Answer.create!(id: 13, answer: 'アンコウ')
-# 夜型 積極的 器用的 衝動的 臨機応変
-Answer.create!(id: 14, answer: '車')
-# 夜型 積極的 器用的 計画的 真面目
-Answer.create!(id: 15, answer: '温泉')
-# 夜型 積極的 器用的 計画的 臨機応変
-Answer.create!(id: 16, answer: '飛行機')
-
-# 朝型 消極的 専門的 衝動的 真面目
-Answer.create!(id: 17, answer: '照明')
-# 朝型 消極的 専門的 衝動的 臨機応変
-Answer.create!(id: 18, answer: 'エアコン')
-# 朝型 消極的 専門的 計画的 真面目
-Answer.create!(id: 19, answer: '観葉植物')
-# 朝型 消極的 専門的 計画的 臨機応変
-Answer.create!(id: 20, answer: '空気清浄機')
-# 朝型 消極的 器用的 衝動的 真面目
-Answer.create!(id: 21, answer: '電子レンジ')
-# 朝型 消極的 器用的 衝動的 臨機応変
-Answer.create!(id: 22, answer: 'クッション')
-# 朝型 消極的 器用的 計画的 真面目
-Answer.create!(id: 23, answer: '時計')
-# 朝型 消極的 器用的 計画的 臨機応変
-Answer.create!(id: 24, answer: 'ポスト')
-# 朝型 積極的 専門的 衝動的 真面目
-Answer.create!(id: 25, answer: '餅')
-# 朝型 積極的 専門的 衝動的 臨機応変
-Answer.create!(id: 26, answer: 'レモン')
-# 朝型 積極的 専門的 計画的 真面目
-Answer.create!(id: 27, answer: '接着剤')
-# 朝型 積極的 専門的 計画的 臨機応変
-Answer.create!(id: 28, answer: 'コンパス')
-# 朝型 積極的 器用的 衝動的 真面目
-Answer.create!(id: 29, answer: '鳥')
-# 朝型 積極的 器用的 衝動的 臨機応変
-Answer.create!(id: 30, answer: 'スマートフォン')
-# 朝型 積極的 器用的 計画的 真面目
-Answer.create!(id: 31, answer: '電車')
-# 朝型 積極的 器用的 計画的 臨機応変
-Answer.create!(id: 32, answer: '郵便')
+Answer.create!(id: 1, answer: 'number1')
+Answer.create!(id: 2, answer: 'number2')
+Answer.create!(id: 3, answer: 'number3')
+Answer.create!(id: 4, answer: 'number4')
+Answer.create!(id: 5, answer: 'number5')
+Answer.create!(id: 6, answer: 'number6')
+Answer.create!(id: 7, answer: 'number7')
+Answer.create!(id: 8, answer: 'number8')
+Answer.create!(id: 9, answer: 'number9')
+Answer.create!(id: 10, answer: 'number10')
+Answer.create!(id: 11, answer: 'number11')
+Answer.create!(id: 12, answer: 'number12')
+Answer.create!(id: 13, answer: 'number13')
+Answer.create!(id: 14, answer: 'number14')
+Answer.create!(id: 15, answer: 'number15')
+Answer.create!(id: 16, answer: 'number16')
+Answer.create!(id: 17, answer: 'number17')
+Answer.create!(id: 18, answer: 'number18')
+Answer.create!(id: 19, answer: 'number19')
+Answer.create!(id: 20, answer: 'number20')
+Answer.create!(id: 11, answer: 'number21')
+Answer.create!(id: 22, answer: 'number22')
+Answer.create!(id: 23, answer: 'number23')
+Answer.create!(id: 24, answer: 'number24')
+Answer.create!(id: 25, answer: 'number25')
+Answer.create!(id: 26, answer: 'number26')
+Answer.create!(id: 27, answer: 'number27')
+Answer.create!(id: 28, answer: 'number28')
+Answer.create!(id: 29, answer: 'number29')
+Answer.create!(id: 30, answer: 'number30')
+Answer.create!(id: 31, answer: 'number31')
+Answer.create!(id: 32, answer: 'number32')
