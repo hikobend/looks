2a7f571f2242a46c507586e12fb83212d6a287cb
hikobend <hiko.bkend@gmail.com>
2022-07-03 21:34:31 +0900

    6この回答

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,69 +1,69 @@
 # 夜型 消極的 専門的 衝動的 真面目
-Answer.create!(id: 1, answer: 'number1')
-# 夜型 消極的 専門的 衝動的 冷静
+Answer.create!(id: 1, answer: 'オーロラ')
+# 夜型 消極的 専門的 衝動的 臨機応変
 Answer.create!(id: 2, answer: 'number2')
 # 夜型 消極的 専門的 計画的 真面目
 Answer.create!(id: 3, answer: '街灯')
-# 夜型 消極的 専門的 計画的 冷静
+# 夜型 消極的 専門的 計画的 臨機応変
 Answer.create!(id: 4, answer: '月')
 # 夜型 消極的 器用的 衝動的 真面目
 Answer.create!(id: 5, answer: 'number5')
-# 夜型 消極的 器用的 衝動的 冷静
+# 夜型 消極的 器用的 衝動的 臨機応変
 Answer.create!(id: 6, answer: 'number6')
 # 夜型 消極的 器用的 計画的 真面目
-Answer.create!(id: 7, answer: 'number7')
-# 夜型 消極的 器用的 計画的 冷静
-Answer.create!(id: 8, answer: 'number8')
+Answer.create!(id: 7, answer: 'フクロウ')
+# 夜型 消極的 器用的 計画的 臨機応変
+Answer.create!(id: 8, answer: '月')
 # 夜型 積極的 専門的 衝動的 真面目
 Answer.create!(id: 9, answer: 'number9')
-# 夜型 積極的 専門的 衝動的 冷静
+# 夜型 積極的 専門的 衝動的 臨機応変
 Answer.create!(id: 10, answer: 'number10')
 # 夜型 積極的 専門的 計画的 真面目
 Answer.create!(id: 11, answer: 'number11')
-# 夜型 積極的 専門的 計画的 冷静
+# 夜型 積極的 専門的 計画的 臨機応変
 Answer.create!(id: 12, answer: 'number12')
 # 夜型 積極的 器用的 衝動的 真面目
 Answer.create!(id: 13, answer: 'number13')
-# 夜型 積極的 器用的 衝動的 冷静
+# 夜型 積極的 器用的 衝動的 臨機応変n
 Answer.create!(id: 14, answer: 'number14')
 # 夜型 積極的 器用的 計画的 真面目
 Answer.create!(id: 15, answer: 'number15')
-# 夜型 積極的 器用的 計画的 冷静
+# 夜型 積極的 器用的 計画的 臨機応変
 Answer.create!(id: 16, answer: 'number16')
 # 朝型 消極的 専門的 衝動的 真面目
-Answer.create!(id: 17, answer: 'number17')
-# 朝型 消極的 専門的 衝動的 冷静
+Answer.create!(id: 17, answer: '照明')
+# 朝型 消極的 専門的 衝動的 臨機応変
 Answer.create!(id: 18, answer: 'number18')
 # 朝型 消極的 専門的 計画的 真面目
 Answer.create!(id: 19, answer: 'number19')
-# 朝型 消極的 専門的 計画的 冷静
+# 朝型 消極的 専門的 計画的 臨機応変
 Answer.create!(id: 20, answer: 'number20')
 # 朝型 消極的 器用的 衝動的 真面目
 Answer.create!(id: 21, answer: 'number21')
-# 朝型 消極的 器用的 衝動的 冷静
+# 朝型 消極的 器用的 衝動的 臨機応変
 Answer.create!(id: 22, answer: 'number22')
 # 朝型 消極的 器用的 計画的 真面目
 Answer.create!(id: 23, answer: 'number23')
-# 朝型 消極的 器用的 計画的 冷静
+# 朝型 消極的 器用的 計画的 臨機応変
 Answer.create!(id: 24, answer: 'number24')
 # 朝型 積極的 専門的 衝動的 真面目
 Answer.create!(id: 25, answer: 'number25')
-# 朝型 積極的 専門的 衝動的 冷静
+# 朝型 積極的 専門的 衝動的 臨機応変
 Answer.create!(id: 26, answer: 'number26')
 # 朝型 積極的 専門的 計画的 真面目
 Answer.create!(id: 27, answer: 'number27')
-# 朝型 積極的 専門的 計画的 冷静
+# 朝型 積極的 専門的 計画的 臨機応変
 Answer.create!(id: 28, answer: 'number28')
 # 朝型 積極的 器用的 衝動的 真面目
 Answer.create!(id: 29, answer: 'number29')
-# 朝型 積極的 器用的 衝動的 冷静
+# 朝型 積極的 器用的 衝動的 臨機応変
 Answer.create!(id: 30, answer: 'number30')
 # 朝型 積極的 器用的 計画的 真面目
 Answer.create!(id: 31, answer: 'number31')
-# 朝型 積極的 器用的 計画的 冷静
+# 朝型 積極的 器用的 計画的 臨機応変
 Answer.create!(id: 32, answer: 'number32')
 
-# 照明
+
 # 飛行機
 # 車
 # クッション
@@ -73,8 +73,6 @@ Answer.create!(id: 32, answer: 'number32')
 # もち
 # 時計
 # スマートフォン
-# フクロウ
 # イルカ
 # アライグマ
-# カバ
-# オーロラ
+# 接着剤
