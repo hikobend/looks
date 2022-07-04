755f49e73cf584ac90273fcd26fe32f759a96701
hikobend <hiko.bkend@gmail.com>
2022-07-04 21:44:29 +0900

    answerのanswer試験

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 class Answer < ApplicationRecord
-  validates :answer, presence: true
+  validates :answer, presence: true, length: { in: 1..60, allow_blank: true }
 end
--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,7 +1,71 @@
 # frozen_string_literal: true
 
-# require 'rails_helper'
+# 単体テスト(UnitTest)
+require 'rails_helper'
 
-# RSpec.describe Answer, type: :model do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
+RSpec.describe Answer, type: :model do
+  describe 'validation' do
+    subject(:answer) { build(:answer) }
+
+    describe 'all data' do
+      context 'すべてのデータが正しい' do
+        it 'データを保存する' do
+          expect(answer.valid?).to eq true
+        end
+      end
+    end
+
+    describe 'answer' do
+      context '入力された名前が正しい' do
+        subject(:answer) { build(:answer, answer: 'テスト') }
+
+        it 'データを保存する' do
+          expect(answer.valid?).to eq true
+        end
+      end
+
+      context '名前がnil' do
+        subject(:answer) { build(:answer, answer: nil) }
+
+        it '保存ができない' do
+          expect(answer.valid?).to eq false
+          expect(answer.errors.messages[:answer]).to include "can't be blank"
+        end
+      end
+
+      context '名前が0文字' do
+        subject(:answer) { build(:answer, answer: '') }
+
+        it '保存できない' do
+          answer = FactoryBot.build(:answer, answer: nil)
+          expect(answer.valid?).to eq false
+          expect(answer.errors.messages[:answer]).to include "can't be blank"
+        end
+      end
+
+      context '名前が1文字' do
+        subject(:answer) { build(:answer, answer: Faker::Lorem.characters(number: 1).to_s) }
+
+        it '保存ができる' do
+          expect(answer.valid?).to eq true
+        end
+      end
+
+      context '名前が60文字' do
+        subject(:answer) { build(:answer, answer: Faker::Lorem.characters(number: 60).to_s) }
+
+        it '保存ができる' do
+          expect(answer.valid?).to eq true
+        end
+      end
+
+      context '名前が61文字' do
+        subject(:answer) { build(:answer, answer: Faker::Lorem.characters(number: 61).to_s) }
+
+        it '保存できない' do
+          expect(answer.valid?).to eq false
+        end
+      end
+    end
+  end
+end
