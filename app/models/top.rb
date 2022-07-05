893177d94e99fc8ba186d5c643c4d1d66bc1bf3b
hikobend <hiko.bkend@gmail.com>
2022-07-05 21:36:42 +0900

    エラーメッセージ編集

--- app/models/top.rb
+++ app/models/top.rb
@@ -11,35 +11,39 @@ class Top < ApplicationRecord
 
   private
 
-  def name_presence
-    return if name.present?
+    def name_presence
+      return if name.present?
 
-    errors.add(:base, "名前を入力してください.")
-  end
+      errors.add(:base, '名前を入力してください.')
+    end
 
-  def ques_one_inclusion
-    return if ques_one.present?
+    def ques_one_inclusion
+      return if ques_one.present?
 
-    errors.add(:base, "1つ目の質問に回答してください")
-  end
-  def ques_two_inclusion
-    return if ques_one.present?
+      errors.add(:base, '1つ目の質問に回答してください')
+    end
 
-    errors.add(:base, "2つ目の質問に回答してください")
-  end
-  def ques_three_inclusion
-    return if ques_one.present?
+    def ques_two_inclusion
+      return if ques_one.present?
 
-    errors.add(:base, "3つ目の質問に回答してください")
-  end
-  def ques_four_inclusion
-    return if ques_one.present?
+      errors.add(:base, '2つ目の質問に回答してください')
+    end
 
-    errors.add(:base, "4つ目の質問に回答してください")
-  end
-  def ques_five_inclusion
-    return if ques_one.present?
+    def ques_three_inclusion
+      return if ques_one.present?
 
-    errors.add(:base, "5つ目の質問に回答してください")
-  end
+      errors.add(:base, '3つ目の質問に回答してください')
+    end
+
+    def ques_four_inclusion
+      return if ques_one.present?
+
+      errors.add(:base, '4つ目の質問に回答してください')
+    end
+
+    def ques_five_inclusion
+      return if ques_one.present?
+
+      errors.add(:base, '5つ目の質問に回答してください')
+    end
 end
